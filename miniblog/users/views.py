7ab0e5d46dc761d45aa8b856e6d8b427e63826c7
Alinda Byamukama <alindabyamukama@gmail.com>
2024-03-30 18:34:23 +0300

    add edit to form import, this creates the form and passes it to our template

--- miniblog/users/views.py
+++ miniblog/users/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.contrib.auth.form import RegisterUserForm
+from django.contrib.auth.forms import RegisterUserForm
 
 # Create your views here.
 def register_user(request):
     # if request.method == 'POST':
-    #     form = RegisterUser(request.POST)
+    form = RegisterUserForm()
         
-    return render(request, 'users/register_user.html')
+    return render(request, 'users/register_user.html', {"form": form})

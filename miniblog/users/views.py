9a7a8d62899da1f2f17a228b84741339ab640d67
Alinda Byamukama <alindabyamukama@gmail.com>
2024-03-30 18:23:35 +0300

    add comments to unused code

--- miniblog/users/views.py
+++ miniblog/users/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+# from users.forms import RegisterUser
+
 # Create your views here.
 def register_user(request):
+    # if request.method == 'POST':
+    #     form = RegisterUser(request.POST)
+        
     return render(request, 'users/register_user.html')

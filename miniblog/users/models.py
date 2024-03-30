bba2eca32221c5eafdcbc2bc5099592cdb0ebb87
Alinda Byamukama <alindabyamukama@gmail.com>
2024-03-30 18:23:18 +0300

    add a users model class

--- miniblog/users/models.py
+++ miniblog/users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    # userID = models.IntegerField()
+    username = models.TextField()
+    email = models.EmailField()
+    # password = models.

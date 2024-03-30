7cc4ded46f2a53c1542ad4ddecdd1495c279800a
Alinda Byamukama <alindabyamukama@gmail.com>
2024-03-30 18:50:02 +0300

    edit replace with UserCreationForm (see docs)

--- miniblog/users/views.py
+++ miniblog/users/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import RegisterUserForm
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def register_user(request):
-    # if request.method == 'POST':
-    form = RegisterUserForm()
-        
+    form = UserCreationForm()        
     return render(request, 'users/register_user.html', {"form": form})

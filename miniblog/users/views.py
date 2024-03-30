6c082a13635f336e942aa10ab5d777c5cfb331ea
Alinda Byamukama <alindabyamukama@gmail.com>
2024-03-30 19:01:42 +0300

    add redirect import, logic to validate form + save if valid + redirect to posts list page

--- miniblog/users/views.py
+++ miniblog/users/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def register_user(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("posts:list")
     form = UserCreationForm()        
     return render(request, 'users/register_user.html', {"form": form})

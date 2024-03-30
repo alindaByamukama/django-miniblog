ecbbef565f73975a9d3a4cdc7c805642d10e6817
Alinda Byamukama <alindabyamukama@gmail.com>
2024-03-30 21:32:32 +0300

    add view for login user form, import AuthenticationForm

--- miniblog/users/views.py
+++ miniblog/users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
 def register_user(request):
@@ -11,3 +11,10 @@ def register_user(request):
     else:
         form = UserCreationForm()        
     return render(request, 'users/register_user.html', {"form": form})
+
+def login_user(request):
+    if request.method == "POST":
+        return redirect("users:login")
+    else:
+        form = AuthenticationForm()
+    return render(request, "users/login.html", {"form": form})

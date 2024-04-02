a1187e77ccfb5c335123dab1fd3e2d58bb1fb9f2
Alinda Byamukama <alindabyamukama@gmail.com>
2024-04-02 14:14:07 +0300

    add blog post serializer - create and return blog posts

--- miniblog/posts/views.py
+++ miniblog/posts/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
-from .models import Post
+from .models import BlogPost
+from rest_framework import generics
+from .serializers import BlogPostSerializer
 
 # Create your views here.
-def posts_list(request):
-    posts = Post.objects.all().order_by('-date')
-    return render(request, 'posts/posts_list.html', {'posts': posts})
+class BlogPostListCreate(generics.ListCreateAPIView):
+    queryset = BlogPost.obejcts.all()
+    serializer_class = BlogPostSerializer
+    
+# def posts_list(request):
+#     posts = Post.objects.all().order_by('-date')
+#     return render(request, 'posts/posts_list.html', {'posts': posts})
 
-def post_page(request, slug):
-    post = Post.objects.get(slug=slug)
-    return render(request, 'posts/post_page.html', {'post': post})
+# def post_page(request, slug):
+#     post = Post.objects.get(slug=slug)
+#     return render(request, 'posts/post_page.html', {'post': post})

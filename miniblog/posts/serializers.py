2ace4cbfc9f1ffb4e11e666e3f0ada7a363fc31a
Alinda Byamukama <alindabyamukama@gmail.com>
2024-04-02 14:11:23 +0300

    remove previous code and add blog post serializer w blogpost model meta

--- miniblog/posts/serializers.py
+++ miniblog/posts/serializers.py
@@ -1,20 +1,7 @@
 from rest_framework import serializers
-from posts.models import Post
+from .models import BlogPost
 
-class PostSerializer(serializers.Serializer):
-    title = serializers.models.CharField(max_length=75)
-    body = serializers.models.TextField()
-    slug = serializers.models.SlugField()
-    date = serializers.models.DateTimeField(auto_now_add=True)
-    # author = serializers.models.ForeignKey(User)
-
-    def create(self, validated_data):
-        return Post.objects.create(**validated_data)
-    
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.body = validated_data.get('body', instance.body)
-        instance.slug = validated_data.get('slug', instance.slug)
-        instance.date = validated_data.get('date', instance.date)
-        # instance.author = validated_data.get('author', instance.author)
-        return instance
+class BlogPostSerializer(serializers.Serializer):
+    class Meta:
+        model = BlogPost
+        fields = ["id", "title", "body", "slug", "date", "author"]

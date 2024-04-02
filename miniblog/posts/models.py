18cd99ffd0762c87a887a99b1181009cdbc937de
Alinda Byamukama <alindabyamukama@gmail.com>
2024-04-02 14:05:07 +0300

    add author field, and return blog post title

--- miniblog/posts/models.py
+++ miniblog/posts/models.py
@@ -1,14 +1,14 @@
 from django.db import models
+from users.models import User
 
 # Create your models here.
-class Post(models.Model):
+class BlogPost(models.Model):
     title = models.CharField(max_length=75)
     body = models.TextField()
     slug = models.SlugField()
     date = models.DateTimeField(auto_now_add=True)
-    # author = models.ForeignKey(User)
+    author = models.ForeignKey(User)
 
     def __str__(self):
         return self.title
         # return f"Post by {self.user.username} at {self.date}"
-    

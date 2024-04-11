62580fce824048819e6d6980bc1892ab5c739eae
Alinda Byamukama <alindabyamukama@gmail.com>
2024-04-11 13:19:04 +0300

    add author foreign key to blog posts model

--- miniblog/api/models.py
+++ miniblog/api/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
 class BlogPost(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     published = models.DateTimeField(auto_now_add=True)
-
+    
+    author = models.ForeignKey('auth,User', related_name='blogposts', on_delete=models.RESTRICT, null=True)
+    
     def __str__(self):
         return self.title

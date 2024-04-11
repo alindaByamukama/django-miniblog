0055131ff6dde33de95b68a450ed0e19b52dba75
Alinda Byamukama <alindabyamukama@gmail.com>
2024-04-11 11:23:27 +0300

    add blog post updates class

--- miniblog/api/views.py
+++ miniblog/api/views.py
@@ -7,3 +7,8 @@ from .serializers import BlogPostSerializer
 class BlogPostListCreate(generics.ListCreateAPIView):
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
+
+class BlogPostRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+    lookup_field = "pk"

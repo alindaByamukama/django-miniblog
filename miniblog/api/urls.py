dd865875c3ccfb4c7d5579bebaa66b9321ba7e62
Alinda Byamukama <alindabyamukama@gmail.com>
2024-04-11 13:59:20 +0300

    edit path to reflect change in user RetrieveAPIView

--- miniblog/api/urls.py
+++ miniblog/api/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('users/', views.UserList.as_view(), name='users-list-create'),
-    path('users/add', views.UserCreate.as_view(), name='users-create'),
+    path('users/<int:pk>', views.UserDetail.as_view(), name='users-detail'),
     path('blogposts/', views.BlogPostListCreate.as_view(), name='blogpost-list-create'),
     path('blogposts/<int:pk>', views.BlogPostRetrieveUpdateDestroy.as_view(), name='blogpost-update'),
     path('blogposts/filter/', views.BlogPostListFilter.as_view(), name='blogpost-filter')

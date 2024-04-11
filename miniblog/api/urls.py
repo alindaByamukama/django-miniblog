6abb8d007ef5f645dcda4f09ffa2e990d75e3bb7
Alinda Byamukama <alindabyamukama@gmail.com>
2024-04-11 15:25:23 +0300

    add edit to path names lines 5, 6

--- miniblog/api/urls.py
+++ miniblog/api/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('users/', views.UserList.as_view(), name='users-list'),
-    path('users/<int:pk>', views.UserDetail.as_view(), name='users-detail'),
+    path('users/', views.UserList.as_view(), name='user-list'),
+    path('users/<int:pk>', views.UserDetail.as_view(), name='user-detail'),
     path('blogposts/', views.BlogPostListCreate.as_view(), name='blogpost-list-create'),
     path('blogposts/<int:pk>', views.BlogPostRetrieveUpdateDestroy.as_view(), name='blogpost-update'),
     path('blogposts/filter/', views.BlogPostListFilter.as_view(), name='blogpost-filter')

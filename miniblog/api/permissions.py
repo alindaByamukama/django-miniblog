925ff005c81d7d77a2cbb9956b2089bfaf7bfe05
Alinda Byamukama <alindabyamukama@gmail.com>
2024-04-11 14:17:18 +0300

    add a custom permission for authors to update or delete posts

---
+++ miniblog/api/permissions.py
@@ -0,0 +1,12 @@
+from rest_framework import permissions
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    # a custom permission to allow authors of an obj to edit
+
+    def has_object_permission(self, request, view, obj):
+        # allow get, head, options reqs
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        # author only
+        return obj.author == request.user

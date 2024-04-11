38d6cada987560987c10de43ff7d8bee4bcd938f
Alinda Byamukama <alindabyamukama@gmail.com>
2024-04-11 17:39:01 +0300

    add url bindings for user list, retrieve

--- miniblog/api/urls.py
+++ miniblog/api/urls.py
@@ -2,7 +2,19 @@ from django.urls import path
 from .views import APIRoot, BlogPostViewSet, UserViewSet
 from rest_framework.urlpatterns import format_suffix_patterns
 
+user_list = UserViewSet.as_view({
+    'get': 'list'
+})
 
+user_detail = UserViewSet.as_view({
+    'get': 'retrieve'
+})
+
+blogpost_list
+
+blogpost_detail
+
+blogpost_filter
 
 urlpatterns = format_suffix_patterns([
     path('', APIRoot),

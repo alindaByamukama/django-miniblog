d7a0945eff26ff6ea4e2119480c8ad471037604f
Alinda Byamukama <alindabyamukama@gmail.com>
2024-04-11 18:20:41 +0300

    delete users app

--- miniblog/users/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Users(models.Model):
-    # userID = models.IntegerField()
-    username = models.TextField()
-    email = models.EmailField()
-    # password = models.

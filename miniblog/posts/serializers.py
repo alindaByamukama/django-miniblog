456b13d229442187f407e988ec73c47e61d6caa0
Alinda Byamukama <alindabyamukama@gmail.com>
2024-04-11 18:20:08 +0300

    delete posts app

--- miniblog/posts/serializers.py
+++
@@ -1,7 +0,0 @@
-from .models import BlogPost
-from rest_framework import serializers
-
-class BlogPostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = BlogPost
-        fields = ["id", "title", "body", "slug", "date"]

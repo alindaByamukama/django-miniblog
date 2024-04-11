2c07ebcf69125799eb48ae355b42f4891bb5f7f8
Alinda Byamukama <alindabyamukama@gmail.com>
2024-04-11 13:40:27 +0300

    add paths for user list and create

--- miniblog/api/urls.py
+++ miniblog/api/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('users/', views.UserList.as_view(), name='users-list-create'),
+    path('users/add', views.UserCreate.as_view(), name='users-create'),
     path('blogposts/', views.BlogPostListCreate.as_view(), name='blogpost-list-create'),
     path('blogposts/<int:pk>', views.BlogPostRetrieveUpdateDestroy.as_view(), name='blogpost-update'),
     path('blogposts/filter/', views.BlogPostListFilter.as_view(), name='blogpost-filter')

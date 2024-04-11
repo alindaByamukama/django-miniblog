26506bab0e15466a4eef2723c660a2372f509512
Alinda Byamukama <alindabyamukama@gmail.com>
2024-04-11 11:45:47 +0300

    add path for blog post list filter

--- miniblog/api/urls.py
+++ miniblog/api/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('blogposts/', views.BlogPostListCreate.as_view(), name='blogpost-list-create'),
-    path('blogposts/<int:pk>', views.BlogPostRetrieveUpdateDestroy.as_view(), name='blogpost-update')
+    path('blogposts/<int:pk>', views.BlogPostRetrieveUpdateDestroy.as_view(), name='blogpost-update'),
+    path('blogposts/filter/', views.BlogPostListFilter.as_view(), name='blogpost-filter')
 ]

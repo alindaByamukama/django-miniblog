037404a3feca5190d76f9f1d0817c1a4c12f63e6
Alinda Byamukama <alindabyamukama@gmail.com>
2024-04-11 15:27:12 +0300

    add format_suffix_patterns to url patterns

--- miniblog/api/urls.py
+++ miniblog/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
-urlpatterns = [
+urlpatterns = format_suffix_patterns([
     path('users/', views.UserList.as_view(), name='user-list'),
     path('users/<int:pk>', views.UserDetail.as_view(), name='user-detail'),
     path('blogposts/', views.BlogPostListCreate.as_view(), name='blogpost-list-create'),
     path('blogposts/<int:pk>', views.BlogPostRetrieveUpdateDestroy.as_view(), name='blogpost-update'),
     path('blogposts/filter/', views.BlogPostListFilter.as_view(), name='blogpost-filter')
-]
+])

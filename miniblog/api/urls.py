ea4fa7e3d3e2115a2a0cbe52474c23dd5c492230
Alinda Byamukama <alindabyamukama@gmail.com>
2024-04-11 17:19:42 +0300

    add api root path

--- miniblog/api/urls.py
+++ miniblog/api/urls.py
@@ -5,6 +5,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 
 urlpatterns = format_suffix_patterns([
+    path('', APIRoot),
     path('users/', views.UserList.as_view(), name='user-list'),
     path('users/<int:pk>', views.UserDetail.as_view(), name='user-detail'),
     path('blogposts/', views.BlogPostList.as_view(), name='blogpost-list'),

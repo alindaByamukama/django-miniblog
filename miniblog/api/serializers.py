a74c390c5eb541fde239b55fe3b5a32b08150b97
Alinda Byamukama <alindabyamukama@gmail.com>
2024-04-11 13:31:52 +0300

    add auth models import user, user serializer

--- miniblog/api/serializers.py
+++ miniblog/api/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from .models import BlogPost
 
 class BlogPostSerializer(serializers.ModelSerializer):
     class Meta:
         model = BlogPost
-        fields = ['id', 'title', 'content', 'published', 'author']
+        fields = ['id', 'title', 'content', 'published']
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email']

a562e4aa67dfe3594e031782342286014ccab137
Alinda Byamukama <alindabyamukama@gmail.com>
2024-04-11 15:58:20 +0300

    add blog post retrieve path

--- miniblog/api/urls.py
+++ miniblog/api/urls.py
@@ -6,6 +6,7 @@ urlpatterns = format_suffix_patterns([
     path('users/', views.UserList.as_view(), name='user-list'),
     path('users/<int:pk>', views.UserDetail.as_view(), name='user-detail'),
     path('blogposts/', views.BlogPostListCreate.as_view(), name='blogpost-list-create'),
+    path('blogposts/details/<int:pk>', views.BlogPostRetrieve.as_view(), name='blogpost-retrieve'),
     path('blogposts/<int:pk>', views.BlogPostRetrieveUpdateDestroy.as_view(), name='blogpost-update'),
     path('blogposts/filter/', views.BlogPostListFilter.as_view(), name='blogpost-filter')
 ])

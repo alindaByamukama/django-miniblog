57d61ed5c3a79fb34a42b94e57e0853f09d5fdd4
Alinda Byamukama <alindabyamukama@gmail.com>
2024-04-11 11:26:11 +0300

    add path for blog post updates

--- miniblog/api/urls.py
+++ miniblog/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('blogposts/', views.BlogPostListCreate.as_view(), name='blogpost-list-create')
+    path('blogposts/', views.BlogPostListCreate.as_view(), name='blogpost-list-create'),
+    path('blogpost/<int:pk>', views.BlogPostRetrieveUpdateDestroy.as_view(), name='blogpost-update')
 ]

5f96483bf762d5e759884e00eafa316a4f7c7dbd
Alinda Byamukama <alindabyamukama@gmail.com>
2024-04-11 11:35:28 +0300

    add delete blog post path with int pk to get a specific blog post

--- miniblog/api/urls.py
+++ miniblog/api/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('blogposts/', views.BlogPostListCreate.as_view(), name='blogpost-list-create'),
-    path('blogpost/<int:pk>', views.BlogPostRetrieveUpdateDestroy.as_view(), name='blogpost-update')
+    path('blogposts/<int:pk>', views.BlogPostRetrieveUpdateDestroy.as_view(), name='blogpost-update')
 ]

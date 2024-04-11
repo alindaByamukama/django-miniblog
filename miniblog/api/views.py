d4ee74bf44099fb103ea32332c55f46e057720d3
Alinda Byamukama <alindabyamukama@gmail.com>
2024-04-11 15:03:30 +0300

    add api root view

--- miniblog/api/views.py
+++ miniblog/api/views.py
@@ -1,13 +1,22 @@
-from django.shortcuts import render
 from rest_framework import generics, status, permissions
 from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework.views import APIView
+
 from django.contrib.auth.models import User
+
 from .models import BlogPost
 from .serializers import BlogPostSerializer, UserSerializer
-from rest_framework.views import APIView
 from .permissions import IsAuthorOrReadOnly
 
 # Create your views here.
+class APIRoot(APIView):
+    def get(self, request, format=None):
+        return Response({
+            'users': reverse('user-list',request=request, format=format),
+            'posts': reverse('post-list', request=request, format=format)
+        })
+
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer

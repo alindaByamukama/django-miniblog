54d667e9f1822f2ce75e024b630e6f83004cec1a
Alinda Byamukama <alindabyamukama@gmail.com>
2024-04-11 13:25:54 +0300

    remove unused code

--- miniblog/miniblog/urls.py
+++ miniblog/miniblog/urls.py
@@ -14,14 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
 from django.urls import path, include
-# from . import views
 
 urlpatterns = [
-    path('', include('api.urls')),
-    # path('', views.homepage),
-    # path('about', views.about),
-    # path('posts/', include('posts.urls')),
-    # path('users/', include('users.urls'))
+    path('', include('api.urls'))
 ]

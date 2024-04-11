f047e74ead44f57baf94f33121c950213a3d6df8
Alinda Byamukama <alindabyamukama@gmail.com>
2024-04-11 16:10:06 +0300

    add change blog posts serializer to a heyper linked model serializer

--- miniblog/api/serializers.py
+++ miniblog/api/serializers.py
@@ -2,11 +2,11 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import BlogPost
 
-class BlogPostSerializer(serializers.ModelSerializer):
+class BlogPostSerializer(serializers.HyperlinkedModelSerializer):
     author = serializers.ReadOnlyField(source='author.username')
     class Meta:
         model = BlogPost
-        fields = ['id', 'title', 'content', 'published', 'author']
+        fields = ['url', 'id', 'title', 'content', 'published', 'author']
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     posts = serializers.HyperlinkedRelatedField(many=True, view_name='blogpost-retrieve', read_only=True)

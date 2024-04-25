24509c18560f3ebcd387622514783d5f362da2ca
Alinda Byamukama <alindabyamukama@gmail.com>
2024-04-25 13:20:08 +0300

    add custom auth token path to url patterns

--- miniblog/api/urls.py
+++ miniblog/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken import views
 
 from api import views
 
@@ -9,5 +10,6 @@ router.register(r'blogposts', views.BlogPostViewSet, basename='blogpost')
 router.register(r'users', views.UserViewSet, basename='user')
 
 urlpatterns = [
+    path('token', views.CustomAuthToken, basename='token'),
     path('', include(router.urls)),
 ]

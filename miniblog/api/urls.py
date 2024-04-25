a9aa904bd038f7a8f38f5971edbc0b9466187307
Alinda Byamukama <alindabyamukama@gmail.com>
2024-04-25 13:13:15 +0300

    add comment to custom auth view route

--- miniblog/api/urls.py
+++ miniblog/api/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 from api import views
 
 router = DefaultRouter()
-router.register(r'token', views.CustomAuthToken, basename='token'),
+# router.register(r'token', views.CustomAuthToken, basename='token'),
 router.register(r'blogposts', views.BlogPostViewSet, basename='blogpost')
 router.register(r'users', views.UserViewSet, basename='user')
 

2267adfb7d2a0304b5ad2e3a5b3f078f74cf638e
Alinda Byamukama <alindabyamukama@gmail.com>
2024-04-25 13:43:17 +0300

    remove auth token details

--- miniblog/api/urls.py
+++ miniblog/api/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from rest_framework.authtoken import views
 
 from api import views
 
@@ -9,6 +8,5 @@ router.register(r'blogposts', views.BlogPostViewSet, basename='blogpost')
 router.register(r'users', views.UserViewSet, basename='user')
 
 urlpatterns = [
-    path('token/', views.obtain_auth_token),
     path('', include(router.urls)),
 ]

abda31701e8d1fedad8b04789fe77f319ad4fca5
Alinda Byamukama <alindabyamukama@gmail.com>
2024-04-25 12:53:43 +0300

    add edit to custom atuh token view resp

--- miniblog/api/views.py
+++ miniblog/api/views.py
@@ -44,6 +44,7 @@ class CustomAuthToken(ObtainAuthToken):
         token, created = Token.objects.get_or_create(user=user)
         return Response({
             'token': token.key,
-            'user_id': user.pk,
+            'user_url': user.pk,
+            'username': user.username,
             'email': user.email
         })

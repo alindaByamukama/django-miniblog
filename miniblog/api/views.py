14261b6b3dfab7b4837e99feb223e33508e46730
Alinda Byamukama <alindabyamukama@gmail.com>
2024-04-25 13:38:39 +0300

    add comment to custom auth token imports, class

--- miniblog/api/views.py
+++ miniblog/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics, status, permissions, viewsets, filters
-from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.authtoken.models import Token
+# from rest_framework.authtoken.views import ObtainAuthToken
+# from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework.views import APIView
@@ -36,15 +36,15 @@ class BlogPostViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-class CustomAuthToken(ObtainAuthToken):
-    def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data, context={'request': request})
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
-        token, created = Token.objects.get_or_create(user=user)
-        return Response({
-            'token': token.key,
-            'user_url': user.pk,
-            'username': user.username,
-            'email': user.email
-        })
+# class CustomAuthToken(ObtainAuthToken):
+#     def post(self, request, *args, **kwargs):
+#         serializer = self.serializer_class(data=request.data, context={'request': request})
+#         serializer.is_valid(raise_exception=True)
+#         user = serializer.validated_data['user']
+#         token, created = Token.objects.get_or_create(user=user)
+#         return Response({
+#             'token': token.key,
+#             'user_url': user.pk,
+#             'username': user.username,
+#             'email': user.email
+#         })

9be04ac223058e76ab8fb7979026ba178856a70e
Alinda Byamukama <alindabyamukama@gmail.com>
2024-04-25 12:46:08 +0300

    add custom auth token view

--- miniblog/api/views.py
+++ miniblog/api/views.py
@@ -33,3 +33,15 @@ class BlogPostViewSet(viewsets.ModelViewSet):
     
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email
+        })

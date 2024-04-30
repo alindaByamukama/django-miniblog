8c94980dba07289e33cfaafae843d6bcdb46c468
Alinda Byamukama <alindabyamukama@gmail.com>
2024-04-30 17:15:30 +0300

    add user model instead of previous model import

--- miniblog/api/models.py
+++ miniblog/api/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.conf import settings
+# from django.conf import settings
 # auto generate tokens for users
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -9,7 +9,7 @@ from rest_framework.authtoken.models import Token
 
 # Create your models here.
 
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+@receiver(post_save, sender=User)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
         Token.objects.create(user=instance)

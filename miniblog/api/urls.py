7bb2a85f3ad127b297ab816c351e91d1ff16de7d
Alinda Byamukama <alindabyamukama@gmail.com>
2024-05-15 16:21:00 +0300

    add login, register routes

--- miniblog/api/urls.py
+++ miniblog/api/urls.py
@@ -6,6 +6,8 @@ from api import views
 router = DefaultRouter()
 router.register(r'blogposts', views.BlogPostViewSet, basename='blogpost')
 router.register(r'users', views.UserViewSet, basename='user')
+router.register(r'login', views.UserLoginViewSet, basename='login')
+router.register(r'register', views.UserRegistrationViewSet, basename='register')
 
 urlpatterns = [
     path('', include(router.urls)),

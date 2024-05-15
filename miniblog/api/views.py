da65be9c56f7db029dcf0f8f2ce176ecdee4c3ca
Alinda Byamukama <alindabyamukama@gmail.com>
2024-05-15 14:09:38 +0300

    add user registration, login viewsets

--- miniblog/api/views.py
+++ miniblog/api/views.py
@@ -1,11 +1,12 @@
 from rest_framework import generics, status, permissions, viewsets, filters
 # from rest_framework.authtoken.views import ObtainAuthToken
-# from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework.views import APIView
 
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 from .models import BlogPost
 from .serializers import BlogPostSerializer, UserSerializer
@@ -35,6 +36,19 @@ class BlogPostViewSet(viewsets.ModelViewSet):
     
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+class UserRegistrationViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny]
+
+class UserLoginView(viewsets.ModelViewSet):
+    def post(self, request):
+        user = authenticate(username=request.data['username'], password=request.data['password'])
+        if user:
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key})
+        else:
+            return Response({'error': 'Invalid credentials'}, status=401)
 
 # class CustomAuthToken(ObtainAuthToken):
 #     def post(self, request, *args, **kwargs):

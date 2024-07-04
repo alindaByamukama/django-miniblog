092bc9a642ca57dde07c9b90bf77102c7786b305
Alinda Byamukama <alindabyamukama@gmail.com>
2024-07-04 21:36:23 +0300

    add linted/formatted code

--- miniblog/api/models.py
+++ miniblog/api/models.py
@@ -6,8 +6,10 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     published = models.DateTimeField(auto_now_add=True)
-    
-    author = models.ForeignKey('auth.User', related_name='blogposts', on_delete=models.RESTRICT, null=True)
-    
+
+    author = models.ForeignKey(
+        "auth.User", related_name="blogposts", on_delete=models.RESTRICT, null=True
+    )
+
     def __str__(self):
         return self.title
--- miniblog/api/serializers.py
+++ miniblog/api/serializers.py
@@ -2,14 +2,20 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import BlogPost
 
+
 class BlogPostSerializer(serializers.HyperlinkedModelSerializer):
-    author = serializers.ReadOnlyField(source='author.username')
+    author = serializers.ReadOnlyField(source="author.username")
+
     class Meta:
         model = BlogPost
-        fields = ['url', 'id', 'title', 'content', 'published', 'author']
+        fields = ["url", "id", "title", "content", "published", "author"]
+
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-    posts = serializers.HyperlinkedRelatedField(many=True, view_name='blogpost-detail', read_only=True)
+    posts = serializers.HyperlinkedRelatedField(
+        many=True, view_name="blogpost-detail", read_only=True
+    )
+
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'posts']
+        fields = ["url", "username", "email", "posts"]

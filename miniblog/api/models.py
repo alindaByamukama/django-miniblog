4b0205520a9f21baa1f274f30867e94cc0a3ae71
Alinda Byamukama <alindabyamukama@gmail.com>
2024-07-04 21:41:47 +0300

    add updates to title and content fields

--- miniblog/api/models.py
+++ miniblog/api/models.py
@@ -3,13 +3,11 @@ from django.db import models
 
 # Create your models here.
 class BlogPost(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
+    title = models.CharField(max_length=100, null=False, blank=False)
+    content = models.TextField(null=False, blank=False)
     published = models.DateTimeField(auto_now_add=True)
 
-    author = models.ForeignKey(
-        "auth.User", related_name="blogposts", on_delete=models.RESTRICT, null=True
-    )
+    author = models.ForeignKey("auth.User", related_name="blogposts", on_delete=models.RESTRICT, null=True)
 
     def __str__(self):
         return self.title

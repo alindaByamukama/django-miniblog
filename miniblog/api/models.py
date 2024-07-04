8a47a405b31abc3bbecfe9a33981dfa617133264
Alinda Byamukama <alindabyamukama@gmail.com>
2024-07-04 21:54:38 +0300

    add an updated at field to track blog post updates

--- miniblog/api/models.py
+++ miniblog/api/models.py
@@ -6,7 +6,7 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=100, null=False, blank=False)
     content = models.TextField(null=False, blank=False)
     published = models.DateTimeField(auto_now_add=True)
-
+    updated_at = models.DateTimeField(auto_now=True)
     author = models.ForeignKey("auth.User", related_name="blogposts", on_delete=models.RESTRICT, null=True)
 
     def __str__(self):

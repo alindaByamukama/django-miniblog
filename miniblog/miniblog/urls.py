84ef074a2e085f75e1c9d8bc4c855c82427afc01
Alinda Byamukama <alindabyamukama@gmail.com>
2024-07-05 16:58:37 +0300

    add schema view

--- miniblog/miniblog/urls.py
+++ miniblog/miniblog/urls.py
@@ -19,6 +19,19 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.urls import path, include
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Your API",
+        default_version='v1',
+        description="Description of your API",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@yourapi.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('', include('api.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))

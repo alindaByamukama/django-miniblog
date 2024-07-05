b48d8842316f2312a576379491610068beb356ac
Alinda Byamukama <59140792+alindaByamukama@users.noreply.github.com>
2024-07-06 00:03:23 +0300

    Update settings.py
    add 'django-miniblog.onrender.com' to ALLOWED_HOSTS

--- miniblog/miniblog/settings.py
+++ miniblog/miniblog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-_cyym4_!os0s!7ae@9)ofb5=^b!_0l3()-y!xqr-56)0$#obi@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['alindab.pythonanywhere.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['alindab.pythonanywhere.com', 'localhost', '127.0.0.1', 'django-miniblog.onrender.com']
 
 # Application definition
 

91e5cbe0a1b8cc20d2b2629f0449d670f4c9e841
Alinda Byamukama <alindabyamukama@gmail.com>
2024-07-07 21:21:11 +0300

    add register serializer - commented for future implementation

--- miniblog/api/serializers.py
+++ miniblog/api/serializers.py
@@ -38,3 +38,22 @@ class UserSerializer(serializers.ModelSerializer):
             instance.set_password(password)
         instance.save()
         return instance
+    
+# class RegisterSerializer(serializers.ModelSerializer):
+#     password2 = serializers.CharField(write_only=True)
+
+#     class Meta:
+#         model = Userfields = ('username', 'email', 'password', 'password2')
+
+#     def save(self, **kwargs):
+#         user = User(
+#             username=self.validated_data['username']
+#             email=self.validated_data['email']
+#         )
+#         password = self.validated_data['password']
+#         password2 = self.validated_data['password2']
+#         if password != password2:
+#             raise serializers.ValidationError({"password": "Passwords must match."})
+#         user.set_password(password)
+#         user.save()
+#         return user

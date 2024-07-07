73437bd9a71f0eb19b32de2a29dddbf97ae91e12
Alinda Byamukama <alindabyamukama@gmail.com>
2024-07-07 17:20:14 +0300

    add Optional: Static files dirs config

--- miniblog/miniblog/settings.py
+++ miniblog/miniblog/settings.py
@@ -144,6 +144,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+# Optional: Static files dirs if you have custom static files location
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 

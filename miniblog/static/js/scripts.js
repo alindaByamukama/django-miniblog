78159c5433a13e3eb4ff61c19d7c7111e571b50f
Alinda Byamukama <alindabyamukama@gmail.com>
2024-07-07 21:53:37 +0300

    add script to update copyright year dynamically

---
+++ miniblog/static/js/scripts.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var yearSpan = document.getElementById('year');
+    var currentYear = new Date().getFullYear();
+    yearSpan.textContent = currentYear;
+});
